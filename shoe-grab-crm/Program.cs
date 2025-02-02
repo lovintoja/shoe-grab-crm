7571d9672c471156ceecbb51ce9291b26d560a10
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-02 03:07:02 +0100

    001: remove fingerprint verification, add swagger on http

--- shoe-grab-crm/Program.cs
+++ shoe-grab-crm/Program.cs
@@ -15,11 +15,11 @@ builder.WebHost.ConfigureKestrel(options =>
         listenOptions.UseHttps("Resources\\server.pfx", "test123", httpsOptions =>
         {
             httpsOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-            httpsOptions.ClientCertificateValidation = (certificate, chain, errors) =>
-            {
-                return certificate.Thumbprint == "ExpectedClientThumbprint";
-            };
         });
+    });
+    options.Listen(IPAddress.Any, 5081, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
     });
 });
 
