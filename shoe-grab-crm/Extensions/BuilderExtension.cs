e9f831cc13908eb3d101fbece005e7fa17514e2f
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-08 00:30:00 +0100

    002: add prod/dev configs, remove email functionality, insert stub

---
+++ shoe-grab-crm/Extensions/BuilderExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ShoeGrabCRM.Extensions;
+
+public static class BuilderExtension
+{
+    public static void SetupKestrel(this WebApplicationBuilder builder)
+    {
+        builder.WebHost.ConfigureKestrel((context, options) =>
+        {
+            var kestrelSection = context.Configuration.GetSection("Kestrel:Endpoints");
+
+            var grpcEndpoint = kestrelSection.GetSection("Grpc");
+            if (grpcEndpoint.Exists())
+            {
+                var grpcUrl = new Uri(grpcEndpoint["Url"]);
+                options.Listen(IPAddress.Any, grpcUrl.Port, listenOptions =>
+                {
+                    listenOptions.Protocols = Enum.Parse<HttpProtocols>(grpcEndpoint["Protocols"]);
+                    listenOptions.UseHttps(httpsOptions =>
+                    {
+                        var certificatePath = grpcEndpoint["Certificate:Path"];
+                        var certificatePassword = grpcEndpoint["Certificate:Password"];
+                        var parseSuccess = Enum.TryParse(grpcEndpoint["Certificate:ClientCertificateMode"], out ClientCertificateMode clientCertificateMode);
+
+                        if (certificatePath != null && certificatePassword != null && parseSuccess)
+                        {
+                            var certificate = new X509Certificate2(certificatePath, certificatePassword);
+                            httpsOptions.ServerCertificate = certificate;
+                            httpsOptions.ClientCertificateMode = clientCertificateMode;
+                        }
+                    });
+                });
+            }
+        });
+    }
+}
--- shoe-grab-crm/Program.cs
+++ shoe-grab-crm/Program.cs
@@ -1,42 +1,34 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Server.Kestrel.Https;
+using ShoeGrabCRM.Extensions;
 using ShoeGrabCRM.Grpc;
-using System.Net;
+using ShoeGrabCRM.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.WebHost.ConfigureKestrel(options =>
+builder.SetupKestrel();
+
+builder.Services.AddScoped<IEmailService, SmtpEmailService>();
+builder.Services.AddCors(options =>
 {
-    options.Listen(IPAddress.Any, 7355, listenOptions =>
+    options.AddPolicy("AllowAllOrigins", policy =>
     {
-        listenOptions.Protocols = HttpProtocols.Http2;
-        listenOptions.UseHttps("Resources\\server.pfx", "test123", httpsOptions =>
-        {
-            httpsOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-        });
-    });
-    options.Listen(IPAddress.Any, 5081, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http1;
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
-
 builder.Services.AddGrpc();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseCors("AllowAllOrigins");
 }
 
 app.UseHttpsRedirection();
